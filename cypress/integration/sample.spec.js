85f83e7c1a0de5c76306a81fa484c5670d97e069
Cahya Eka Permana <43898558+cahyaekapermana@users.noreply.github.com>
2021-08-07 11:38:43 +0700

    testing lupa password url

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -31,9 +31,29 @@ describe('User melakukan proses login pada SIAKAD Polinema', () => {
 
   it('User mengklik tombol lupa password', () => {
     cy.visit('http://siakad.polinema.ac.id/')
-    cy.get('.forget-password').click()
+    
+    cy.contains('Lupa Password?').click()
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', 'https://helpakademik.polinema.ac.id/ajukan-pertanyaan')
 
   })
+
+  // id lupa password
+  // forget-password
+  
+  // it('Gets, types and asserts', () => {
+  //   cy.visit('https://example.cypress.io')
+
+  //   cy.contains('type').click()
+
+  //   // Should be on a new URL which includes '/commands/actions'
+  //   cy.url().should('include', '/commands/actions')
+
+  //   // Get an input, type into it and verify that the value has been updated
+  //   cy.get('.action-email')
+  //     .type('fake@email.com')
+  //     .should('have.value', 'fake@email.com')
+  // })
 
 
 
